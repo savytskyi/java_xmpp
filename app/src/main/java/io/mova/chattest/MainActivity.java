b62f29663f6734e2971919ddaae51cf78dbf28d0
Kyrylo Savytskyi <mail@savytskyi.com>
2016-03-22 10:03:12 +0200

    init

---
+++ app/src/main/java/io/mova/chattest/MainActivity.java
@@ -0,0 +1,16 @@
+package io.mova.chattest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        XMPPTCPConnectionConfiguration.Builder config = XMPPTCPConnectionConfiguration.builder();
+    }
+}
